3b7a5e7acdb9c338e96fa89d5c3c07e57524104f
deepakphpweb <dipakrana2760@gmail.com>
2021-12-12 19:07:57 +0530

    Added : Welcome To Address Book System and attached .gitignore

---
+++ src/com/bridge/abook/AddressBookMain.java
@@ -0,0 +1,52 @@
+package com.bridge.abook;
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    //Declare Variable
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    int zip;
+    long phoneNumber;
+    String emailId;
+
+    //Take Input Value
+    private void getInput() {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first name : ");
+        this.firstName=sc.next();
+        System.out.print("Enter the last name : ");
+        this.lastName=sc.next();
+        System.out.print("Enter the address : ");
+        this.address=sc.next();
+        System.out.print("Enter the city name : ");
+        this.city=sc.next();
+        System.out.print("Enter the zip code : ");
+        this.zip=sc.nextInt();
+        System.out.print("Enter the phone number : ");
+        this.phoneNumber=sc.nextLong();
+        System.out.print("Enter the email id : ");
+        this.emailId=sc.next();
+    }
+
+    //Show the Values
+    private void showAddressBook() {
+        System.out.println("First Name : "+firstName);
+        System.out.println("Last Name : "+lastName);
+        System.out.println("Address : "+address);
+        System.out.println("City Name : "+city);
+        System.out.println("Zip code : "+zip);
+        System.out.println("Phone Number : "+phoneNumber);
+        System.out.println("Email Id : "+emailId);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+        AddressBookMain addBookMain=new AddressBookMain();
+        addBookMain.getInput();
+        addBookMain.showAddressBook();
+    }
+
+}
